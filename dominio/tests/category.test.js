15b10419e929acf6a386343336ee4e684db0db6a
GuilleRamirez <guillermo.ramirez@tiendamia.com>
2021-11-20 01:21:11 -0300

    clean code

--- dominio/tests/category.test.js
+++ dominio/tests/category.test.js
@@ -11,21 +11,10 @@ let ute = new Category(categoryData);
 
 test("obtener nombre de categoría", () => {
     expect(ute.name).toBe("luz");
-});
-
-test("obtener tipó del categoría", () => {
     expect(ute.type).toBe("egreso");
-});
-
-test("obtener color de categoría", () => {
     expect(ute.color).toBe("rojo");
-});
-
-test("obtener cantidad elementos de categoría", () => {
     expect(ute.cantElem).toBe(1);
 });
-
-
 test("setear nombre de categoría", () => {
     ute.sumarCantidad();
     expect(ute.cantElem).toBe(2);
